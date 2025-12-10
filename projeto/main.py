2c7a6309478efe9a7ba301ac971b00ed6700722f
Nicolle Prione <nprione@proton.me>
2025-12-10 20:07:35 -0300

    Leitura da planilha

--- projeto/main.py
+++ projeto/main.py
@@ -1,24 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import xlrd
 import time
 
-arquivo = open('Resultado', 'w')
+book = xlrd.open_workbook('/home/nprione/Documentos/Meus_Projetos/Meu/Projeto 1 - Modelo do curso OficinaPython/dominios.xls')
+planilha = book.sheet_by_name('Plan1')
+linhas = planilha.nrows
+
+arquivo = open('Resultado.txt', 'w')
 
 navegador = webdriver.Chrome()
 navegador.get('https://registro.br')
-
-dominio = 'testesite'
-barra_pesquisa = navegador.find_element(By.ID, 'is-avail-field')
-barra_pesquisa.clear()
-barra_pesquisa.send_keys(dominio)
-barra_pesquisa.send_keys(Keys.RETURN)
 time.sleep(2)
 
-status = navegador.find_element(By.XPATH, '//*[@id="conteudo"]/div/section/div[2]/div/p/span/strong')
-
-arquivo.write(f'Domínio = ({dominio}) // Status = {status.text}')
+for item in range(0, linhas):
+    x = planilha.cell_value(item, 0)
+    barra_pesquisa = navegador.find_element(By.ID, 'is-avail-field')
+    time.sleep(2)
+    barra_pesquisa.clear()
+    barra_pesquisa.send_keys(x)
+    barra_pesquisa.send_keys(Keys.RETURN)
+    time.sleep(2)
+    status = navegador.find_element(By.XPATH, '//*[@id="conteudo"]/div/section/div[2]/div/p/span/strong')
+    arquivo.write(f'Domínio = ({x}) // Status = {status.text}. \n')
+    time.sleep(1)
+    
 arquivo.close()
-
-time.sleep(2)
 navegador.quit()
